20807a3fd3d9a99e8958c30bcf1c0325c57901e4
mor2711 <61913635+mor2711@users.noreply.github.com>
2020-04-03 17:59:15 -0400

    Initial commit

---
+++ venv/Generate.py
@@ -0,0 +1,33 @@
+import random
+def generate_cred(n):
+    file1 =open(n,"r")
+
+    database=[]
+    for i in file1.readlines():
+        lastname = i.split()[1]
+        if len(lastname) > 5:
+            part1 = str(lastname)[0:5]
+        else:
+            part1 = lastname
+        initial = i[0]
+        username = part1 + initial
+        counter = 0
+        if username not in database:
+            username = username
+        else:
+            username = username + str(counter)
+            counter = counter + 1
+        database.append(username)
+        pool = "abcdefghijklmnopqrstuvwxyz"
+        pool2 = pool + pool.upper()
+        pool3 = pool2 + "1234567890!@#$%^&*()"
+        randomcharacter = [random.choice(pool3) for i in range(8)]
+        password = "".join(randomcharacter)
+        output = "username {} password {}".format(username,password)
+        print(output)
+        file2 =open("ad","a")
+        file2.writelines(output + "\n")
+        file2.close()
+    return;
+generate_cred("list1.txt")
+generate_cred("list2.txt")
